de1458dbd62baea47ada87cda36eaee733187aa4
arash bahreini <vbhost.ir@gmail.com>
2019-01-27 21:25:26 +0330

    clean project

--- AkoSatrap/obj/Release/Package/PackageTmp/app/factory/validationFactory.js
+++
@@ -1,9 +0,0 @@
-﻿mainApp.factory('validationFactory', function () {
-
-    var serviceObj = {};
-
-    serviceObj.setValidationState = function (objectId, state, message) {
-        ui_validation_state(objectId, state, message);
-    };
-    return serviceObj;
-});
